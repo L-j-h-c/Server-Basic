481f0e2175e0f67e7ce83b20d03f5b1a2a6aa4bd
L-j-h-c <ckrgkswnsgh@naver.com>
2022-01-27 02:05:08 +0900

    [Study] 자료형
    자료형

--- 1stSeminar/variable.js
+++ 1stSeminar/variable.js
@@ -61,3 +61,59 @@ function hoistFunction() {
 }
 함수 안에서 선언부를 가장 위로 끌어올려서 해석된다.*/
 
+/* 자료형
+원시 타입(Primitive Type)Number, String, Boolean, Null, Undefined, Symbol
+객체 타입(Object Type) : Object
+Number : 자바스크립의 숫자 타입은 단 하나, Number이다. 모든 수는 64bit 실수로 표현됨.
+Strinfg : 문자열 자료형, (")와 (')를 동일하게 취급, ES6부터 백틱 문자열 'Template literal'을 지원
+Symbol : 유일하고 변경할 수 없는 식별자를 만들고 싶을 때 사용
+Null : 값이 정해지지 않은 것(타입은 있음), Object타입
+Undefined : 타입이 정해지지 않은 것, 초기화되지 않은 변수나 존재하지 않는 값
+*/
+
+/* Object
+자바스크립트의 기본 타입, {}로 감싸진 형태이다.
+property의 정렬되지 않은 집합
+Property는 key:value의 형태로 구성된다.
+property가 함수일 경우 Method라고 부름
+원시타입을 제외한 나머지는 모두 객체 -> function, array, regex 모두 객체
+*/
+
+const animal = {
+    animalType: "dog",
+    animatName: "뽀삐",
+    animalFriends: ["코코", "초코", "쿠키"],
+    bark: function () {
+        HTMLFormControlsCollection.log(`${this.animalName}: 멍멍`);
+    },
+    thisFriends: function () {
+        this.animalFriends.forEach(friend => {
+            console.log(`${this.animalName}의 친구: ${friend}`);
+        });
+    },
+};
+
+/* Array
+JS에선 array도 객체
+배열 요소의 타입이 고정되어 있지 않음 -> 같은 배열에 있는 요소끼리 타입이 다를 수도 있음
+배열 요소의 인덱스가 연속적이지 않아도 됨 -> 따라서 특정 배열 요소가 비어 있을 수도 있음
+기본 형태 : arr = [elem1, elem2, elem3]
+*/
+
+// 예시
+let arr = [2, 'some string', undefined];
+let arr = Array(null, 4, {part: 'server'});
+
+/* Function
+JS에선 function도 객체!
+하나의 고유한 목적의 작업을 수행하도록 설계된 독립적인 블록
+자바스크립트의 함수는 "일급 객체"
+- 변수, 데이터 구조에 담을 수 있다
+- 다른 함수의 파라미터로 전달할 수 있다
+- 반환 값으로 사용할 수 있다
+*/
+
+function someAwesomeFunction(parameters) {
+    // 함수 본문
+}
+someAwesomeFunction
